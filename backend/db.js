d58c1e9177b135c7f591346047df67c3baae210c
catheriny09 <catheriny09@gmail.com>
2024-02-26 21:35:26 -0800

    adding username and website fields

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@ const db = new sqlite3.Database('./passwordManager.db');
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS passwords (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    website TEXT NOT NULL,
+    username TEXT NOT NULL,
     password TEXT NOT NULL
   )`);
 });
