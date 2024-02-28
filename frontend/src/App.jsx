610d2e6b475d0c467a75c8a2406b94ee6b5eed8d
catheriny09 <catheriny09@gmail.com>
2024-02-27 23:48:23 -0800

    updating input form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,45 +2,68 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [website, setWebsite] = useState('');
-  const [username, setUsername] = useState('');
   const [passwords, setPasswords] = useState([]);
   const [newPassword, setNewPassword] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newWebsite, setNewWebsite] = useState('');
 
   useEffect(() => {
-    fetchCredentials();
+    fetchPasswords();
   }, []);
 
   const fetchPasswords = async () => {
     const response = await axios.get('http://localhost:3000/passwords');
-    setCredentials(response.data);
+    setPasswords(response.data);
   };
 
   const addPassword = async () => {
-    await axios.post('http://localhost:3000/passwords', { website, username, password: newPassword });
+    await axios.post('http://localhost:3000/passwords', { 
+      password: newPassword, 
+      username: newUsername, 
+      website: newWebsite 
+    });
     setNewPassword('');
-    setWebsite('');
-    setUsername('');
+    setNewUsername('');
+    setNewWebsite('');
     fetchPasswords();
   };
 
   const deletePassword = async (id) => {
     await axios.delete(`http://localhost:3000/passwords/${id}`);
-    fetchCredentials();
+    fetchPasswords();
   };
 
   return (
     <div>
-      <input 
-        type="text" 
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)} 
-      />
+      <div>
+        <input 
+          type="text" 
+          placeholder="Website"
+          value={newWebsite}
+          onChange={(e) => setNewWebsite(e.target.value)} 
+        />
+      </div>
+      <div>
+        <input 
+          type="text" 
+          placeholder="Username"
+          value={newUsername}
+          onChange={(e) => setNewUsername(e.target.value)} 
+        />
+      </div>
+      <div>
+        <input 
+          type="password" 
+          placeholder="Password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)} 
+        />
+      </div>
       <button onClick={addPassword}>Add Password</button>
       <ul>
         {passwords.map(password => (
           <li key={password.id}>
-            {password.password} 
+            {password.website} | {password.username}: {password.password} 
             <button onClick={() => deletePassword(password.id)}>Delete</button>
           </li>
         ))}
